743e89ab38b8a292dcb376b21b8ef720c1c2826f
Loctarogar <Morilon@protonmail.com>
2025-04-30 17:55:19 +0200

    waterfall animation added

--- core/src/main/java/com/badlogic/drop/Main.java
+++ core/src/main/java/com/badlogic/drop/Main.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -13,11 +15,15 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture welcomeScreen;
     private Texture baseGround;
+    private Texture wtf1;
 
     enum GameState {
         WELCOME,
         PLAYING
     }
+
+    Animation<TextureRegion> backgroundAnimation;
+    float animationTime = 0f;
 
     GameState currentState = GameState.WELCOME;
 
@@ -26,10 +32,19 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
         welcomeScreen = new Texture("welcome_screen.png");
         baseGround = new Texture("base_ground.png");
+        TextureRegion[] frames = new TextureRegion[6];
+        for (int i = 0; i < 6; i++) {
+            Texture tex = new Texture("waterfall/wtf" + (i + 1) + ".png");
+            frames[i] = new TextureRegion(tex);
+        }
+        backgroundAnimation = new Animation<>(0.1f, frames);
+        backgroundAnimation.setPlayMode(Animation.PlayMode.LOOP);
     }
 
     @Override
     public void render() {
+        animationTime += Gdx.graphics.getDeltaTime();
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         if (currentState == GameState.WELCOME) {
             renderWelcomeScreen();
@@ -45,6 +60,7 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         welcomeScreen.dispose();
+        baseGround.dispose();
     }
 
     public void renderWelcomeScreen(){
@@ -55,6 +71,8 @@ public class Main extends ApplicationAdapter {
 
     public void renderGame() {
         batch.begin();
+        TextureRegion currentFrame = backgroundAnimation.getKeyFrame(animationTime);
+        batch.draw(currentFrame, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(baseGround, 0, 0, Gdx.graphics.getWidth(), (float) Gdx.graphics.getHeight() /5);
         batch.end();
     };
